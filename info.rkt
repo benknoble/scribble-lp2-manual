97103586af16f3d2ee02d336fa537a4396f25d49
D. Ben Knoble <ben.knoble+github@gmail.com>
2021-10-31 13:11:27 -0400

    info: update run-time dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "at-exp-lib"))
+(define deps '("base" "at-exp-lib" "scribble-lib"))
 (define build-deps '("scribble-lib" "scribble-doc" "racket-doc"))
 (define pkg-desc "Literate programming with style")
 (define version "1.0")
